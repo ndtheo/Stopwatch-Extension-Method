9c332332c7a6ed54ae831141b564abcf709b48ad
ndtheo <58115166+ndtheo@users.noreply.github.com>
2019-12-14 07:00:39 +0200

    Add files via upload

---
+++ ExtensionMethods.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace Helpers
+{
+    public static class ExtensionMethods
+    {
+        /// <summary>
+        /// This method facilitates the use of <see cref="Stopwatch"/> class, given that it automatically stops the timer, prints the performance measurement results directly to the Output window (milliseconds, ticks), using <see cref="Debug.WriteLine()"/> method.
+        /// 
+        /// Furthermore, the method can restart the timer if applicable (depending on the restartAfterFinish parameter value).
+        /// </summary>
+        /// <param name="timer"></param>
+        /// <param name="methodPoint"></param>
+        /// <param name="restartAfterFinish"></param>
+        public static void StopWatchAndWriteLineResults(this Stopwatch timer, string methodPoint, bool restartAfterFinish = false)
+        {
+            timer.Stop();
+            Debug.WriteLine($"{methodPoint} finish ms:{timer.ElapsedMilliseconds} ticks:{timer.ElapsedTicks}");
+            if (restartAfterFinish)
+            {
+                timer.Start();
+            }
+        }
+    }
+}
